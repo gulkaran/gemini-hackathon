9ad8abbed255ddc940a2a484219b004bb5f3a950
Gulkaran Singh <82789205+gulkaran@users.noreply.github.com>
2024-04-21 00:56:41 -0400

    Initial Commit

---
+++ gemini-hackathon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gemini-hackathon
+//
+//  Created by gulkaran s on 2024-04-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gemini-hackathon/gemini_hackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  gemini_hackathonApp.swift
+//  gemini-hackathon
+//
+//  Created by gulkaran s on 2024-04-21.
+//
+
+import SwiftUI
+
+@main
+struct gemini_hackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
