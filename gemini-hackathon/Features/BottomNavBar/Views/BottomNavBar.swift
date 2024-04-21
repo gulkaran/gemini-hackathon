257e517ef62ed35f76d416ae6df13a50b2c42677
gulkaran <gulkaransingh.m@gmail.com>
2024-04-21 01:29:34 -0400

    updated

---
+++ gemini-hackathon/Features/BottomNavBar/Views/BottomNavBar.swift
@@ -0,0 +1,18 @@
+//
+//  BottomNavBar.swift
+//  gemini-hackathon
+//
+//  Created by gulkaran s on 2024-04-21.
+//
+
+import SwiftUI
+
+struct BottomNavBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BottomNavBar()
+}
