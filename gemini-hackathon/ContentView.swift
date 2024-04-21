f7ff8e2bfd934bc967701fe1a2eb1349dddef3d9
gulkaran <gulkaransingh.m@gmail.com>
2024-04-21 01:31:30 -0400

    updated

--- gemini-hackathon/ContentView.swift
+++ gemini-hackathon/ContentView.swift
@@ -10,12 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "gamecontroller.fill")
+                .foregroundColor(Color(.pink))
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello world!")
         }
         .padding()
+        BottomNavBar()
     }
 }
 
--- gemini-hackathon/Features/BottomNavBar/Views/BottomNavBar.swift
+++ gemini-hackathon/Features/BottomNavBar/Views/BottomNavBar.swift
@@ -8,8 +8,60 @@
 import SwiftUI
 
 struct BottomNavBar: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            Page1().tabItem {
+                Image(systemName:"cup.and.saucer.fill")
+                Text("Food Tracker")
+            }
+            Page2().tabItem {
+                Image(systemName:"figure.outdoor.cycle")
+                Text("Exercise")
+            }
+            Page3().tabItem {
+                Image(systemName: "sun.horizon.fill")
+                Text("Today")
+            }
+            Page4().tabItem {
+                Image(systemName: "pills.fill")
+                Text("Prescriptions")
+            }
+            Page5().tabItem {
+                Image(systemName: "person.fill")
+                Text("Profile")
+            }
+        }
+    }
+    
+}
+
+struct Page1: View {
+    var body: some View {
+        Text("Page 1")
+    }
+}
+
+struct Page2: View {
+    var body: some View {
+        Text("Page 2")
+    }
+}
+
+struct Page3: View {
+    var body: some View {
+        Text("Page 3")
+    }
+}
+struct Page4: View {
+    var body: some View {
+        Text("Page 4")
+    }
+}
+
+struct Page5: View {
+    var body: some View {
+        Text("Page 5")
     }
 }
 
