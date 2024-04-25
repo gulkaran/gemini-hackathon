06ca51bc6b8d953fb839eb1a748525eb6a6414a3
gulkaran <gulkaransingh.m@gmail.com>
2024-04-24 22:27:59 -0400

    added select food-tracker times view

--- gemini-hackathon/Features/FoodTracker/WelcomeNewUser/Views/Questionaire.swift
+++ gemini-hackathon/Features/FoodTracker/WelcomeNewUser/Views/Questionaire.swift
@@ -8,11 +8,84 @@
 import SwiftUI
 
 struct Questionaire: View {
+    
+    @State private var breakfastTime = Calendar.current.date(bySettingHour: 8, minute: 0, second: 0, of: Date()) ?? Date()
+    @State private var lunchTime = Calendar.current.date(bySettingHour: 12, minute: 0, second: 0, of: Date()) ?? Date()
+    @State private var dinnerTime = Calendar.current.date(bySettingHour: 18, minute: 0, second: 0, of: Date()) ?? Date()
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationView {
+            VStack {
+                VStack(alignment: .leading) {
+                    Text("Reminder Times")
+                        .font(.title)
+                        .bold()
+                        .padding(.bottom, 3)
+                    Text("Set your meal times here. We'll send you a reminder near these times to snap a picture of your meal.")
+                }.padding()
+                
+                HStack {
+                    VStack(alignment: .leading, spacing: 15) {
+                        HStack {
+                            Image(systemName: "cup.and.saucer")
+                            Text("Breakfast:")
+                                .font(.title2)
+                        }
+                        
+                        HStack {
+                            Image(systemName: "takeoutbag.and.cup.and.straw")
+                            Text("Lunch:")
+                                .font(.title2)
+                        }
+                        
+                        HStack {
+                            Image(systemName: "wineglass")
+                            Text("Dinner:")
+                                .font(.title2)
+                        }
+                    }
+                    .padding(.trailing, 50)
+                    
+                    VStack(alignment: .trailing) {
+                        DatePicker("", selection: $breakfastTime, displayedComponents: .hourAndMinute)
+                            .labelsHidden()
+                        
+                        DatePicker("", selection: $lunchTime, displayedComponents: .hourAndMinute)
+                            .labelsHidden()
+                        
+                        DatePicker("", selection: $dinnerTime, displayedComponents: .hourAndMinute)
+                            .labelsHidden()
+                    }
+                }
+                
+                NavigationLink(destination: Next().navigationBarHidden(true)) {
+                        Text("Done")
+                    .padding(EdgeInsets(top: 5, leading: 50, bottom: 5, trailing: 50))
+
+                }
+                
+                    .buttonStyle(.borderedProminent)
+                    .tint(.pink)
+                    .foregroundColor(.white)
+                    .buttonBorderShape(.capsule)
+                    .padding()
+            }
+        }
+        
+        
+        
+        
     }
 }
 
 #Preview {
     Questionaire()
+}
+
+struct Next: View {
+    var body: some View {
+        Text("home page")
+    }
 }
